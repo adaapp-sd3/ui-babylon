7eaf3783fb5b3c53ed2d39123cb84ab23a9d8b6b
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-08-20 15:57:54 +0100

    Add react-intro code

---
+++ babylon-react/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+import "./App.css"
+
+import Welcome from './pages/Welcome'
+import Chatbot from './pages/Chatbot'
+
+function App() {
+
+  const [initialMessage, setInitialMessage] = useState(null)
+
+  return (
+    <>
+      <a href="#chatbot" title="Skip to content">Skip to content</a>
+      <main>
+
+        {!initialMessage && (
+          <Welcome setInitialMessage={setInitialMessage} />
+        )}
+        {initialMessage && (
+          <Chatbot initialMessage={initialMessage} />
+        )}
+        
+      </main>
+    </>
+  )
+}
+
+export default App
